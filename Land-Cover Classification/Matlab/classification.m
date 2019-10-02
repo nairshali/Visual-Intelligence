b00d0368b503f4aff6bd3410f72c25d52a67ffe9
Shalini Nair <47002450+nairshali@users.noreply.github.com>
2019-10-03 00:23:05 +0100

    read pixel value for respective coordinates
    Loop for each class and read pixel value for the respective image from using the coordinates

--- Land-Cover Classification/Matlab/classification.m
+++ Land-Cover Classification/Matlab/classification.m
@@ -12,3 +12,18 @@ nir_img = imread("C:\LandCoverImages\nir.dib");
 
 %% coordinates for each class
 stats = regionprops(labelled_ground_truth, 'PixelList');
+
+% loop for 4 classes
+for k = 1:4
+    
+    % loop for each co-ordinates, per class from each images
+    for i = 1:3
+        r_arr(i,1) = r_img(stats(k).PixelList(i,2), stats(k).PixelList(i,1));
+        g_arr(i,1) = g_img(stats(k).PixelList(i,2), stats(k).PixelList(i,1));
+        b_arr(i,1) = b_img(stats(k).PixelList(i,2), stats(k).PixelList(i,1));
+        fe_arr(i,1) = fe_img(stats(k).PixelList(i,2), stats(k).PixelList(i,1));
+        le_arr(i,1) = le_img(stats(k).PixelList(i,2), stats(k).PixelList(i,1));
+        nir_arr(i,1) = nir_img(stats(k).PixelList(i,2), stats(k).PixelList(i,1));
+    end
+    
+ end
