4dc73731f0e5e53580857cc4ef2fc8d94ca289bb
Shalini Nair <47002450+nairshali@users.noreply.github.com>
2019-10-02 23:52:25 +0100

    Incorrect file location
    Replacing with proper file location, can have file at different location but to make it more clean

--- Land-Cover Classification/Matlab/classification.m
+++ Land-Cover Classification/Matlab/classification.m
@@ -2,10 +2,10 @@
 clear all; close all; clc;
 
 %% Read Images
-load('D:\LandCoverImages\ground_truth.mat');
-r_img = imread("D:\LandCoverImages\r.dib");
-g_img = imread("D:\LandCoverImages\g.dib");
-b_img = imread("D:\LandCoverImages\b.dib");
+load('C:\LandCoverImages\ground_truth.mat');
+r_img = imread("C:\LandCoverImages\r.dib");
+g_img = imread("C:\LandCoverImages\g.dib");
+b_img = imread("C:\LandCoverImages\b.dib");
 fe_img = imread("C:\LandCoverImages\fe.dib");
 le_img = imread("C:\LandCoverImages\le.dib");
 nir_img = imread("C:\LandCoverImages\nir.dib");
