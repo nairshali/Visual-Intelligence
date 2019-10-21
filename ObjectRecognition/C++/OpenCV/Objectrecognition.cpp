66b52a98e139a213f31bdbb86aad4c2f2af8f39f
Shalini Nair <47002450+nairshali@users.noreply.github.com>
2019-10-21 20:15:24 +0100

    function to draw crosshairs on your tracked image

--- ObjectRecognition/C++/OpenCV/Objectrecognition.cpp
+++ ObjectRecognition/C++/OpenCV/Objectrecognition.cpp
@@ -162,6 +162,40 @@ void loadLabels(string &labelpathName, vector<string> &labels) {
 		createTrackbar("V_MAX", trackbarWindowName, &V_MAX, V_MAX, on_trackbar);
 	}
 
+// draw crosshairs on your tracked image!
+void drawObject(int x, int y, Mat &frame) {
+
+//use some of the openCV drawing functions to draw crosshairs
+//on your tracked image!
+
+//UPDATE:JUNE 18TH, 2013
+//added 'if' and 'else' statements to prevent
+//memory errors from writing off the screen (ie. (-25,-25) is not within the window!)
+
+	circle(frame, Point(x, y), 20, Scalar(0, 255, 0), 2);
+	if (y - 25 > 0)
+		line(frame, Point(x, y), Point(x, y - 25), Scalar(0, 255, 0), 2);
+	else 
+		line(frame, Point(x, y), Point(x, 0), Scalar(0, 255, 0), 2);
+	
+	if (y + 25 < FRAME_HEIGHT)
+		line(frame, Point(x, y), Point(x, y + 25), Scalar(0, 255, 0), 2);
+	else 
+		line(frame, Point(x, y), Point(x, FRAME_HEIGHT), Scalar(0, 255, 0), 2);
+	
+	if (x - 25 > 0)
+		line(frame, Point(x, y), Point(x - 25, y), Scalar(0, 255, 0), 2);
+	else 
+		line(frame, Point(x, y), Point(0, y), Scalar(0, 255, 0), 2);
+	if (x + 25 < FRAME_WIDTH)
+		line(frame, Point(x, y), Point(x + 25, y), Scalar(0, 255, 0), 2);
+	else 
+		line(frame, Point(x, y), Point(FRAME_WIDTH, y), Scalar(0, 255, 0), 2);
+
+	putText(frame, intToString(x) + "," + intToString(y), Point(x, y + 30), 1, 1, Scalar(0, 255, 0), 2);
+
+}
+
 //HOGDescriptor hog(Size(640, 480), Size(16, 16), Size(8, 8), Size(8, 8), 9);
 HOGDescriptor hog;
 
